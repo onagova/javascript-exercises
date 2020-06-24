f98945fbfa35cf9de77a86dde8723cf044c6856e
Tanat Cheurthai <naepanda@hotmail.com>
2020-06-24 14:48:14 +0700

    Complete removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++) {
+        while (true) {
+            const j = array.indexOf(arguments[i]);
 
+            if (j < 0) break;
+
+            array.splice(j, 1);
+        }
+    }
+
+    return array;
 }
 
 module.exports = removeFromArray
