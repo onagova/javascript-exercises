86b0f75650df1653511c1bd27bd32e6d70bb9b55
Tanat Cheurthai <naepanda@hotmail.com>
2020-06-24 14:48:59 +0700

    Complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9;
+  return roundToOneDecimal(celsius);
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit = celsius * 9 / 5 + 32;
+  return roundToOneDecimal(fahrenheit);
 }
 
 module.exports = {
   ftoc,
   ctof
+}
+
+function roundToOneDecimal(n) {
+  n *= 10;
+  n = Math.round(n);
+  return n / 10;
 }
