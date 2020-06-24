39afa86df0a8e4e185ef55e9b163fcd6bada054c
Tanat Cheurthai <naepanda@hotmail.com>
2020-06-24 14:48:45 +0700

    Complete leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 4) ? false :
+        (year % 100) ? true :
+        (year % 400) ? false :
+        true;
 }
 
 module.exports = leapYears
