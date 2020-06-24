1cae4acfb5526d9948979d00e7f1a9210033cb8b
Tanat Cheurthai <naepanda@hotmail.com>
2020-06-24 14:47:21 +0700

    Complete repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, iteration) {
+    if (iteration < 0) return 'ERROR';
 
+    let result = '';
+
+    for (let i = 0; i < iteration; i++) {
+        result += string;
+    }
+
+    return result;
 }
 
 module.exports = repeatString
