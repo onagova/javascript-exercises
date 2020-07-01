d2fa9e3c27b906a5b59a9b856c5ca81a4e81c774
Tanat Cheurthai <naepanda@hotmail.com>
2020-07-01 10:24:24 +0700

    Complete fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // const fib = [1, 1];
 
+    // for (let i = 2; i < n; i++) {
+    //     fib.push(fib[i - 1] + fib[i - 2]);
+    // }
+
+    // return fib[n - 1];
+
+    if (n < 0) return 'OOPS';
+
+    return (n < 3) ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = fibonacci
