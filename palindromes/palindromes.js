b280fd7aa6b09d58a8364d77844c9e9cac0807fc
Tanat Cheurthai <naepanda@hotmail.com>
2020-07-01 09:13:33 +0700

    Complete palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,'');
+    string = string.toLowerCase();
 
+    const halfLength = Math.floor(string.length / 2);
+
+    for (let i = 0; i < halfLength; i++) {
+        if (string[i] != string[string.length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 module.exports = palindromes
