10452dccbdef3f2d976c3b34f3d46918da44dcf2
Tanat Cheurthai <naepanda@hotmail.com>
2020-07-01 08:44:30 +0700

    Complete calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,31 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((total, item) => total + item, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce((total, item) => total * item, 1);
 }
 
-function power() {
-	
+function power(a, b) {
+	return a ** b;	
 }
 
-function factorial() {
-	
+function factorial(n) {
+	let result = 1;
+
+	for (let i = 2; i <= n; i++) {
+		result *= i;
+	}
+
+	return result;
 }
 
 module.exports = {
