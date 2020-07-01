ff37c649da12574cc3d0507b6421546d32f0e4cb
Tanat Cheurthai <naepanda@hotmail.com>
2020-07-01 10:45:29 +0700

    Complete findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    return people.sort((a, b) => {
+        const aAge = getAge(a.yearOfBirth, a.yearOfDeath);
+        const bAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        return (aAge < bAge) ? 1 : -1;
+    })[0];
+}
 
+function getAge(birth, death) {
+    death = death ?? (new Date()).getFullYear();
+    return death - birth;
 }
 
 module.exports = findTheOldest
