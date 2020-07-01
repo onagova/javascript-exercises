532b8240ab16b3df16dbd07ca74d0ae3d763bf4d
Tanat Cheurthai <naepanda@hotmail.com>
2020-07-01 10:13:09 +0700

    Complete caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,33 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    const upperA = 'A'.charCodeAt(0);
+    const upperZ = 'Z'.charCodeAt(0);
+    const lowerA = 'a'.charCodeAt(0);
+    const lowerZ = 'z'.charCodeAt(0);
+    const range = 1 + upperZ - upperA;
 
+    let encrypted = '';
+
+    for (let i = 0; i < string.length; i++) {
+        if (/[a-z]/i.test(string[i])) {
+            const charCode = string.charCodeAt(i);
+
+            const floor = (charCode >= upperA && charCode <= upperZ) ? upperA : lowerA;
+
+            let absoluteShift = charCode - floor + shift;
+            absoluteShift %= range;
+            if (absoluteShift < 0) {
+                absoluteShift += range;
+            }
+
+            const shiftedCode = floor + absoluteShift;
+
+            encrypted += String.fromCharCode(shiftedCode);
+        } else {
+            encrypted += string[i];
+        }
+    }
+
+    return encrypted;
 }
 
 module.exports = caesar
